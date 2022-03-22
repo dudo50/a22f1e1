74a493eb8fdaee22a5037311299d4c6f3b01c572
Dusan Morhac <55763425+dudo50@users.noreply.github.com>
2022-03-22 22:57:19 +0100

    register login and signout

--- models/User.js
+++ models/User.js
@@ -1,8 +1,14 @@
 import mongoose from "mongoose";
 
+
 const userSchema = new mongoose.Schema({
     username:{type:String, required:true, unique:true, trim:true},
     email:{type:String, required:true, unique:true, trim:true},
+    password:{type:String, required:true, trim:true},
+    profilePicture:{type:String, required:true, default:"./assets/profilePic/default.jpg"},
+    status:{type:String, enum:['ACTIVE', 'INACTIVE', 'DISABLED'], required:true, default:'INACTIVE'},
+    user_id:{type:String, required:true, unique:true, trim:true},
+
 })
 
 const UsrMdl = mongoose.model("user", userSchema)
