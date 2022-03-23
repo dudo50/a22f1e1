0ce196cfc9d33905063b7fd9e1a81aa2eef86459
Lupus the Fallen <scientistricmed@gmail.com>
2022-03-23 16:33:46 +0100

    Update userController.js - registering user gets new user_id

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,9 +22,10 @@ class UserController {
     static handleRegister = async (req, res) => {
         try {
             //QUERY FOR HIGHEST USER ID
+            const query_result = await UsrMdl.find().sort({'user_id': -1}).select('user_id').limit(1)   // zored tabulku podla user_id, vrat iba 1 prvok
+            const nove_id = String(Number(query_result[0]['user_id']) + 1);     // Prehod hodnotu na cislo, +1, preved naspat na string
 
-
-            if( await UsrMdl.create({ username: req.params.username, password : req.params.password, email:req.params.email, user_id: "44"}))
+            if( await UsrMdl.create({ username: req.params.username, password : req.params.password, email:req.params.email, user_id: nove_id}))
             //PO REGISTRACII HNED LOGNEME
             await UsrMdl.updateOne({ $and: [ {username: req.params.username}, {password : req.params.password }]}, {status:"ACTIVE"})
             
