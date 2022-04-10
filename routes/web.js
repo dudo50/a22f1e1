66b0fb0734fbfdbab9e6334b889c9a32aef7772f
Dusan Morhac <55763425+dudo50@users.noreply.github.com>
2022-04-10 13:17:52 +0200

    Update web.js

--- routes/web.js
+++ routes/web.js
@@ -50,7 +50,7 @@ router.get('/reviews/:gameId', ReviewController.getSpecificGameReviews); //TESTE
 router.route("/upload/picture/:userId&:randomParam").post(upload,  UserController.updatePhoto) //TESTED FINE
 router.get("/picture/:userId", UserController.getPhoto) //TESTED FINE //USED IN FRONT END
 router.route("/upload/game/:gameId").post(uploadg,  GameController.updateGamePhoto) //TESTED FINE
-router.get("/gamepicture/:gameId&::randomParam", GameController.getGamePhoto) //TESTED FINE
+router.get("/gamepicture/:gameId&:randomParam", GameController.getGamePhoto) //TESTED FINE
 module.exports = router;
 
 //firebase
